5dbd2a8f4b0249f02c53db17b77f6e394e811bc4
Luca Bazzea <luca.bazzea@gmail.com>
2020-02-25 20:11:11 +0200

    fixed crash when user inputs a str

--- guess-the-number.py
+++ guess-the-number.py
@@ -2,20 +2,28 @@ import random
 
 number = random.randint(1,30)
 print("Number range between 1 and 30")
+print("---")
 # print(number)
 
 tries = 0
 
-while tries < 5:
+while tries < 6:
 
-    guess = input("Guess the number: ")
-    guess = int(guess)
+    while True:
+        try:
+            guess = int(input("Guess the number: "))
+            break
+        except:
+            print("Input only accepts decimal numbers")
+            print("---")
 
     tries += 1
     if guess > number:
         print("Nope, Too high")
+        print("---")
     elif guess < number:
         print("No, too low")
+        print("---")
     elif guess == number:
         break
 
@@ -24,6 +32,5 @@ if guess == number:
 elif guess != number:
     print(f"Wrong, the number was {number}")
 
-# Stop game from crashing if a str is entered
 # Add an option to choose how big the number range is
-# Add floating quit or try again menu
+# Add a quit or try again menu
