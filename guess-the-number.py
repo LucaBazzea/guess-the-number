07233758a911fd15cd55066ef8c677b73fc2cd54
Luca Bazzea <luca.bazzea@gmail.com>
2020-02-26 11:58:22 +0200

    Added play again menu

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,36 +1,54 @@
 import random
 
-number = random.randint(1,30)
-print("Number range between 1 and 30")
-print("---")
-# print(number)
+def end_menu():
 
-tries = 0
+    play_again = " "
 
-while tries < 6:
+    while play_again != "y" and play_again != "n":
+        play_again = input("Would you like to play again? [y/n]: ")
 
-    while True:
-        try:
-            guess = int(input("Guess the number: "))
-            break
-        except:
-            print("Input only accepts decimal numbers")
+        if play_again == "y":
+            game()
+        else:
+            quit
+
+def game():
+
+    number = random.randint(1,30)
+    print("Number range between 1 and 30")
+    print("---")
+    # print(number)
+
+    tries = 0
+
+    while tries < 6:
+
+        while True:
+            try:
+                guess = int(input("Guess the number: "))
+                break
+            except:
+                print("Input only accepts decimal numbers")
+                print("---")
+
+        tries += 1
+        if guess > number:
+            print("Nope, Too high")
             print("---")
+        elif guess < number:
+            print("No, too low")
+            print("---")
+        elif guess == number:
+            break
 
-    tries += 1
-    if guess > number:
-        print("Nope, Too high")
-        print("---")
-    elif guess < number:
-        print("No, too low")
-        print("---")
-    elif guess == number:
-        break
+    if guess == number:
+        print("Correct!")
+    elif guess != number:
+        print(f"Wrong, the number was {number}")
+    
+    end_menu()
 
-if guess == number:
-    print("Correct!")
-elif guess != number:
-    print(f"Wrong, the number was {number}")
+game()
 
 # Add an option to choose how big the number range is
 # Add a quit or try again menu
